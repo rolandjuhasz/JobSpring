c6fcabdeb49c34ba3902a0f7c2dd3eb7e3afd6f5
Juhasz Roland <jszrl2003@gmail.com>
2024-11-22 20:43:23 +0100

    back button

---
+++ src/components/BackButton.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { RouterLink } from 'vue-router';
+
+</script>
+
+<template>
+       <section>
+      <div class="container m-auto py-6 px-6">
+        <a
+          href="jobs.html"
+          class="text-green-500 hover:text-green-600 flex items-center"
+        >
+          <i class="fas fa-arrow-left mr-2"></i> Back to Job Listings
+        </a>
+      </div>
+    </section>
+</template>

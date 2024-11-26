61da8968ab1ee20e2122eb9d2f5392ff7177db78
Juhasz Roland <jszrl2003@gmail.com>
2024-11-26 20:00:28 +0100

    desing

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -9,7 +9,7 @@
 </script>
 
 <template>
-    <nav class="bg-green-700 border-b border-green-500">
+    <nav class="bg-pink-900 border-b border-green-500">
       <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
         <div class="flex h-20 items-center justify-between">
           <div
@@ -17,24 +17,24 @@
           >
             <!-- Logo -->
             <RouterLink class="flex flex-shrink-0 items-center mr-4" to="/">
-              <img class="h-10 w-auto" :src="logo" alt="Vue Jobs" />
+              <!-- <img class="h-10 w-auto" :src="logo" alt="Vue Jobs" /> -->
               <span class="hidden md:block text-white text-2xl font-bold ml-2"
-                >Vue Jobs</span
+                >JobSpring</span
               >
             </RouterLink>
             <div class="md:ml-auto">
               <div class="flex space-x-2">
                 <RouterLink
                   to="/"
-                  :class="[isActiveLink('/')  ? 'bg-green-900' : 'hover:bg-gray-900 hover:text-white', 'text-white', 'px-3', 'py-2', 'rounded-md']"
+                  :class="[isActiveLink('/')  ? 'text-orange-300' : 'hover:text-gray-900 hover:text-white', 'text-white', 'px-3', 'py-2', 'rounded-md']"
                   >Home</RouterLink>
                   <RouterLink
                   to="/jobs"
-                  :class="[isActiveLink('/jobs')  ? 'bg-green-900' : 'hover:bg-gray-900 hover:text-white', 'text-white', 'px-3', 'py-2', 'rounded-md']"
+                  :class="[isActiveLink('/jobs')  ? 'text-orange-300' : 'hover:text-gray-900 hover:text-white', 'text-white', 'px-3', 'py-2', 'rounded-md']"
                   >Jobs</RouterLink>
                   <RouterLink
                   to="/jobs/add"
-                  :class="[isActiveLink('/jobs/add')  ? 'bg-green-900' : 'hover:bg-gray-900 hover:text-white', 'text-white', 'px-3', 'py-2', 'rounded-md']"
+                  :class="[isActiveLink('/jobs/add')  ? 'text-orange-300' : 'hover:text-gray-900 hover:text-white', 'text-white', 'px-3', 'py-2', 'rounded-md']"
                   >Add jobs</RouterLink>
               </div>
             </div>

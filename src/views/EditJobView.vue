b1d447e34d322417b575f2590e68cacfe716886d
Juhasz Roland <jszrl2003@gmail.com>
2024-11-26 19:36:21 +0100

    edit function

--- src/views/EditJobView.vue
+++ src/views/EditJobView.vue
@@ -1,8 +1,13 @@
 <script setup>
 import router from '@/router';
-import { reactive } from 'vue';
+import { reactive, onMounted } from 'vue';
+import { useRoute } from 'vue-router';
 import axios from 'axios';
 import { useToast } from 'vue-toastification';
+
+const route = useRoute();
+
+const jobId = route.params.id;
 
 const form = reactive({
     type: 'Full-Time',
@@ -18,10 +23,15 @@ const form = reactive({
     }
 });
 
+const state = reactive({
+    job: {},
+    isLoading: true
+});
+
 const toast = useToast();
 
 const handleSubmit = async () => {
-    const newJob = {
+    const updatedJob = {
         title: form.title,
         type: form.type,
         location: form.location,
@@ -35,14 +45,36 @@ const handleSubmit = async () => {
         },
     };
     try {
-    const response = await axios.post('/api/jobs', newJob);
-    toast.success('Job Added Successfully');
+    const response = await axios.put(`/api/jobs/${jobId}`, updatedJob);
+    toast.success('Job Updated Successfully');
     router.push(`/jobs/${response.data.id}`);
     } catch (error) {
-        toast.error('Job Was Not Added')
+        toast.error('Job Was Not Edited')
         console.error('Error fetching job', error)
 }
 };
+
+onMounted(async () => {
+    try {
+        const response = await axios.get(`/api/jobs/${jobId}`)
+        state.job = response.data;
+
+        form.type = state.job.type;
+        form.title = state.job.title;
+        form.description = state.job.description;
+        form.salary = state.job.salary;
+        form.location = state.job.location;
+        form.company.name = state.job.company.name;
+        form.company.description = state.job.company.description;
+        form.company.contactEmail = state.job.company.contactEmail;
+        form.company.contactPhone = state.job.company.contactPhone;
+    } catch (error) {
+        console.error('Error fetching job', error);
+        
+    } finally {
+        state.isLoading = false;
+    }
+});
 
 </script>
 
@@ -53,7 +85,7 @@ const handleSubmit = async () => {
           class="bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0"
         >
           <form @submit.prevent="handleSubmit">
-            <h2 class="text-3xl text-center font-semibold mb-6">Add Job</h2>
+            <h2 class="text-3xl text-center font-semibold mb-6">Edit Job</h2>
 
             <div class="mb-4">
               <label for="type" class="block text-gray-700 font-bold mb-2"
@@ -212,7 +244,7 @@ const handleSubmit = async () => {
                 class="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline"
                 type="submit"
               >
-                Add Job
+                Update Job
               </button>
             </div>
           </form>
--- src/views/JobView.vue
+++ src/views/JobView.vue
@@ -2,10 +2,13 @@
 import PulseLoader from 'vue-spinner/src/PulseLoader.vue';
 import axios from 'axios';
 import { reactive, onMounted } from 'vue';
-import { useRoute, RouterLink } from 'vue-router';
+import { useRoute, RouterLink, useRouter } from 'vue-router';
 import BackButton from '@/components/BackButton.vue';
+import { useToast } from 'vue-toastification';
 
 const route = useRoute();
+const router = useRouter();
+const toast = useToast();
 
 const jobId = route.params.id
 
@@ -13,6 +16,20 @@ const state = reactive({
   job: {},
   isLoading: true
 });
+
+const deleteJob = async() => {
+  try {
+    const confirm = window.confirm('Are you sure you want to delete this job?')
+    if(confirm) {
+      await axios.delete(`/api/jobs/${jobId}`);
+      toast.success('Job Deleted Successfully');
+      router.push('/jobs')
+    }
+  } catch {
+    console.error('Error deleting job', error)
+    toast.error('Job Not Deleted')
+  }
+}
 
 onMounted(async () => {
     try {
@@ -95,7 +112,7 @@ onMounted(async () => {
                 class="bg-green-500 hover:bg-green-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
                 >Edit Job</RouterLink
               >
-              <button
+              <button @click="deleteJob"
                 class="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
               >
                 Delete Job

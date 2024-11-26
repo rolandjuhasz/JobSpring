9550fd132c533607390117b6936b0817e7f77dc0
Juhasz Roland <jszrl2003@gmail.com>
2024-11-26 20:04:50 +0100

    test

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -14,17 +14,17 @@ defineProps({
 </script>
 
 <template>
-    <section class="bg-green-700 py-20 mb-4">
+    <section class="bg-gray-100 py-20 mb-4">
       <div
         class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-center"
       >
         <div class="text-center">
           <h1
-            class="text-4xl font-extrabold text-white sm:text-5xl md:text-6xl"
+            class="text-4xl font-extrabold text-pink-900 sm:text-5xl md:text-6xl"
           >
             {{title}}
           </h1>
-          <p class="my-4 text-xl text-white">
+          <p class="my-4 text-xl text-black font-bold">
             {{subtitle}}
           </p>
         </div>
